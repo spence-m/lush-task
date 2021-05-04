a49aca04df2215d9f2c2680c7793b1cf5fb9e89a
Matthew Spencer <spencer.ma@protonmail.com>
2021-05-04 20:58:44 +0100

    Added tailwindcss.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "../styles/globals.css";
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
